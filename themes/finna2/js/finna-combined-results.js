63eeb1a322c6e8e943048395cc6ef804677bfc9f
Ere Maijala <ere.maijala@helsinki.fi>
2021-01-15 14:44:29 +0200

    Fix deduplication handling in Versions tab and combined results.

--- themes/finna2/js/finna-combined-results.js
+++ themes/finna2/js/finna-combined-results.js
@@ -4,6 +4,7 @@ finna.combinedResults = (function finnaCombinedResults() {
     init: function init(container) {
       finna.layout.initTruncate();
       finna.openUrl.initLinks(container);
+      finna.itemStatus.initDedupRecordSelection(container);
       VuFind.itemStatuses.check(container);
       finna.record.initRecordVersions(container);
       VuFind.lightbox.bind(container);
